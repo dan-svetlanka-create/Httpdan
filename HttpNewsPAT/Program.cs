68c7c09cae83988d5e454657bf1cf8f060b15974
alexeys6969 <alexshashin.2019@gmail.com>
2025-12-16 14:32:48 +0500

    Добавьте файлы проекта.

---
+++ HttpNewsPAT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace HttpNewsPAT
+{
+    internal class Program
+    {
+        
+    }
+}
